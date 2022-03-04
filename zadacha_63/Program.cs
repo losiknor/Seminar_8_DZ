c215edb6c03bbab951419e1014b531a359f4a7f1
Larina <Losiknor@gmail.com>
2022-03-05 00:04:36 +0700

    Решение задачи 63

--- zadacha_63/Program.cs
+++ zadacha_63/Program.cs
@@ -1,4 +1,19 @@
 ﻿// Задача 63: Сформировать трёхмерный массив с неповторяющимися 
 // двузначными числами и вывести его на экран построчно, с индексами элементов.
-int[,,] array3D = new int[5,4,4];
+int[,,] array3D = new int[5, 4, 4];
+int valueToFill = 9;
 for (int i = 0; i < 5; i++)
+{
+    for (int j = 0; j < 4; j++)
+    {
+        for (int k = 0; k < 4; k++)
+        {
+            array3D[i,j,k] = valueToFill + 1;
+            valueToFill++;
+            Console.Write($"элемент[{i},{j},{k}]{array3D[i,j,k]}  ");
+        }
+    Console.WriteLine("");
+    }
+Console.WriteLine("");
+Console.WriteLine("");
+}
