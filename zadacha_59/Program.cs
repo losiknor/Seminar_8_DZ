f79ecc567aed46f278ab1e25d180deb72757997a
Larina <Losiknor@gmail.com>
2022-03-04 23:24:17 +0700

    Решение задачи 59

--- zadacha_59/Program.cs
+++ zadacha_59/Program.cs
@@ -1,2 +1,31 @@
 ﻿// Задача 59: В прямоугольной матрице найти строку с наименьшей суммой элементов.
-Console.WriteLine("Hello, World!");
+int[,] array = new int[new Random().Next(3, 10), new Random().Next(3, 10)];
+Console.WriteLine("Задана прямоугольная матрица:");
+Console.WriteLine($"Строк {array.GetLength(0)}; столбцов {array.GetLength(1)}");
+int[] arraySum = new int[array.GetLength(0)];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    arraySum[i] = 0;
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(10, 99);
+        Console.Write(array[i, j] + " ");
+        arraySum[i] = arraySum[i] + array[i, j];
+    }
+    Console.Write($"  Сумма строки:  {arraySum[i]}");
+    Console.WriteLine("");
+}
+Console.WriteLine("");
+int minSum = arraySum[0];
+int indexMin = 0;
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    if (arraySum[i] < minSum)
+    {
+        minSum = arraySum[i];
+        indexMin = i;
+    }
+}
+Console.Write("Строка с наименьшей суммой элементов: ");
+for (int j = 0; j < array.GetLength(1); j++)
+    Console.Write(array[indexMin, j] + " ");
